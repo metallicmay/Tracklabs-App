8c2a44816c47af1ce79d8b22d24faa07b9886157
metallicmay <metallicmay@gmail.com>
2015-05-19 00:14:58 +0530

    Create delete_row.php

---
+++ delete_row.php
@@ -0,0 +1,11 @@
+<?php
+	include_once("database.php");
+    $postdata = file_get_contents("php://input");
+	$data = json_decode($postdata);
+	if (property_exists($data, "selected")) {
+		$con = mysql_connect('localhost','root','') or die("Cannot connect :" . mysql_error()); 
+		mysql_select_db("tracklabs",$con) or die("Can't connect :" . mysql_error()); 
+		$sql="DELETE FROM myplaces WHERE name = '" . $data -> name . "'";
+		$query=mysql_query($sql);
+	}
+?>
