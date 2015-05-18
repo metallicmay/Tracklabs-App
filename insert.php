70b1b6a0af1a7fb34a21832e61cc6338ba33b518
metallicmay <metallicmay@gmail.com>
2015-05-18 21:08:53 +0530

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,34 @@
+<?php
+ include_once("database.php");
+$name = $_GET['name'];
+$address = $_GET['address'];
+$lat = $_GET['lat'];
+$lng = $_GET['lng'];
+$user_email = $_GET['email'];
+
+$conn=mysql_connect ("localhost", "root","");
+if (!$conn) {
+  die('Not connected : ' . mysql_error());
+}
+$db = mysql_select_db("tracklabs", $conn);
+if (!$db) {
+  die ('Can\'t use db : ' . mysql_error());
+}
+$sname = mysql_real_escape_string($name);
+$saddr = mysql_real_escape_string($address);
+$slat = mysql_real_escape_string($lat);
+$slng = mysql_real_escape_string($lng);
+$semail = mysql_real_escape_string($user_email);
+
+$query="INSERT INTO myplaces (name,address,lat,lng,user_email)
+            VALUES ('$sname','$saddr','$slat','$slng','$semail')";
+$result = mysql_query($query);
+if (!$result) {
+  die('Invalid query: ' . mysql_error());
+}
+else
+{
+die('Successfully added');
+}
+?>
+
