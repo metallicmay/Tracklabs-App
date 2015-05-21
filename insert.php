ac42b54c2deb05f55479789814dff3940e0bd92e
metallicmay <metallicmay@gmail.com>
2015-05-21 20:03:59 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -4,7 +4,10 @@ $name = $_GET['name'];
 $address = $_GET['address'];
 $lat = $_GET['lat'];
 $lng = $_GET['lng'];
+$url = $_GET['url'];
 $user_email = $_GET['email'];
+
+$urlen=urlencode($url);
 
 $conn=mysql_connect ("localhost", "root","");
 if (!$conn) {
@@ -18,10 +21,11 @@ $sname = mysql_real_escape_string($name);
 $saddr = mysql_real_escape_string($address);
 $slat = mysql_real_escape_string($lat);
 $slng = mysql_real_escape_string($lng);
+$surl = mysql_real_escape_string($urlen);
 $semail = mysql_real_escape_string($user_email);
 
-$query="INSERT INTO myplaces (name,address,lat,lng,user_email)
-            VALUES ('$sname','$saddr','$slat','$slng','$semail')";
+$query="INSERT INTO myplaces (name,address,lat,lng,url,user_email)
+            VALUES ('$sname','$saddr','$slat','$slng','$surl','$semail')";
 $result = mysql_query($query);
 if (!$result) {
   die('Invalid query: ' . mysql_error());
