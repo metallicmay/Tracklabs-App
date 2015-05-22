4eb633c416c21c65fe2e3cce8189592c51815ec4
metallicmay <metallicmay@gmail.com>
2015-05-22 16:41:01 +0530

    Update tracklabs.sql

--- tracklabs.sql
+++ tracklabs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2015 at 10:29 PM
+-- Generation Time: May 22, 2015 at 01:10 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.5.24
 
@@ -31,6 +31,7 @@ CREATE TABLE IF NOT EXISTS `myplaces` (
   `address` varchar(80) NOT NULL,
   `lat` float(10,6) NOT NULL,
   `lng` float(10,6) NOT NULL,
+  `url` varchar(200) NOT NULL,
   `user_email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -38,16 +39,16 @@ CREATE TABLE IF NOT EXISTS `myplaces` (
 -- Dumping data for table `myplaces`
 --
 
-INSERT INTO `myplaces` (`name`, `address`, `lat`, `lng`, `user_email`) VALUES
-('Kochi', 'Kochi, Kerala, India', 9.931233, 76.267303, 'metallicmay@gmail.com'),
-('Bengaluru', 'Bengaluru, Karnataka, India', 12.971599, 77.594566, 'metallicmay@gmail.com'),
-('Chennai', 'Chennai, Tamil Nadu, India', 13.082680, 80.270721, 'metallicmay@gmail.com'),
-('San Francisco', 'San Francisco, CA, USA', 37.774929, -122.419418, 'gayugovind@gmail.com'),
-('Chicago', 'Chicago, IL, USA', 41.878113, -87.629799, 'gayugovind@gmail.com'),
-('Ithaca', 'Ithaca, NY, USA', 42.443962, -76.501884, 'metallicmay@gmail.com'),
-('Delhi', 'Delhi, ON, Canada', 42.852695, -80.499191, 'gayugovind@gmail.com'),
-('Seattle', 'Seattle, WA, USA', 47.606209, -122.332069, 'gayugovind@gmail.com'),
-('Edinburgh', 'Edinburgh, City of Edinburgh, UK', 55.953251, -3.188267, 'metallicmay@gmail.com');
+INSERT INTO `myplaces` (`name`, `address`, `lat`, `lng`, `url`, `user_email`) VALUES
+('Kochi', 'Kochi, Kerala, India', 9.931233, 76.267303, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D9.931233%2C76.267304', 'uaravindshenoy@gmail.com'),
+('Trichy', 'Trichy, Tiruchirappalli, Tamil Nadu, India', 10.808379, 78.646965, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D10.808379%2C78.646966', 'uaravindshenoy@gmail.com'),
+('Dubai', 'Dubai - Dubai - United Arab Emirates', 25.204849, 55.270782, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D25.204849%2C55.270783', 'uaravindshenoy@gmail.com'),
+('Edinburgh', 'Edinburgh, City of Edinburgh, UK', 55.953251, -3.188267, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D55.953252%2C-3.188267', 'metallicmay@gmail.com'),
+('Colorado Springs', 'Colorado Springs, CO, USA', 38.833881, -104.821365, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D38.833882%2C-104.821363', 'metallicmay@gmail.com'),
+('Bengaluru', 'Bengaluru, Karnataka, India', 12.971599, 77.594566, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D12.971599%2C77.594563', 'metallicmay@gmail.com'),
+('San Jose', 'San Jose, CA, USA', 37.338207, -121.886330, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D37.338208%2C-121.886329', 'gayugovind@gmail.com'),
+('Tirunelveli', 'Tirunelveli, Tamil Nadu, India', 8.730000, 77.699997, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D8.73%2C77.7', 'gayugovind@gmail.com'),
+('Georgia', 'Georgia, USA', 32.165623, -82.900078, 'https%3A%2F%2Fmaps.google.com%2Fmaps%3Fll%3D32.165622%2C-82.900075', 'gayugovind@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -62,7 +63,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_email` varchar(100) NOT NULL,
   `user_dob` date NOT NULL,
   `user_num` bigint(10) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -70,17 +71,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_pass`, `user_email`, `user_dob`, `user_num`) VALUES
 (1, 'Maya', 'nitt100', 'metallicmay@gmail.com', '1995-02-28', 9840852548),
-(2, 'Gayu', 'tswift', 'gayugovind@gmail.com', '1995-08-27', 8220296198);
+(2, 'Gayu', 'tswift', 'gayugovind@gmail.com', '1995-08-27', 8220296198),
+(4, 'Shenoy', 'lotr25', 'uaravindshenoy@gmail.com', '1994-11-24', 8148224360);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `myplaces`
---
-ALTER TABLE `myplaces`
-  ADD UNIQUE KEY `lat` (`lat`), ADD UNIQUE KEY `lng` (`lng`);
 
 --
 -- Indexes for table `users`
@@ -96,7 +92,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
