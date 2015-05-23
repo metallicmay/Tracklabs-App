f380c1aabaa25471f39ec556e651b8a1cb1403d6
metallicmay <metallicmay@gmail.com>
2015-05-23 19:20:45 +0530

    Update delete_row.php

--- delete_row.php
+++ delete_row.php
@@ -5,7 +5,7 @@ $data = json_decode($postdata);
     	if (property_exists($data, "selected")) {
 		$con = mysql_connect('localhost','root','') or die("Cannot connect :" . mysql_error()); 
 		mysql_select_db("tracklabs",$con) or die("Can't connect :" . mysql_error()); 
-		$sql="DELETE FROM myplaces WHERE name = '" . $data -> name . "'";
+		$sql="DELETE FROM myplaces WHERE name = '" . $data -> name . "' AND user_email = '" . $data -> email . "'";
 		$query=mysql_query($sql);
 	}
 ?>
